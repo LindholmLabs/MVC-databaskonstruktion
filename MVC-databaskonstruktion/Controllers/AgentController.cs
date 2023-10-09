56861b45c2df8764811ee770bf85c0df6727b72e
William Lindholm <a22willi@student.his.se>
2023-10-09 09:49:40 +0200

    Added AgentController

---
+++ MVC-databaskonstruktion/Controllers/AgentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_databaskonstruktion.Controllers
+{
+    public class AgentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
