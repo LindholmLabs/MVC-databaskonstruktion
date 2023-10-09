3a2847a6d6cc1270b688f7e20e4e5fce639d5267
William Lindholm <a22willi@student.his.se>
2023-10-09 11:43:02 +0200

    Changed default controller to AgentsController.

--- MVC-databaskonstruktion/Program.cs
+++ MVC-databaskonstruktion/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Agents}/{action=Index}/{id?}");
 
 app.Run();
