9c3a0e2ee5224bb4e61ceaaa069cd1068d8eb47e
William Lindholm <a22willi@student.his.se>
2023-10-09 11:06:46 +0200

    Implemented databaseconnection, fetches agents.

--- MVC-databaskonstruktion/Controllers/AgentsController.cs
+++ MVC-databaskonstruktion/Controllers/AgentsController.cs
@@ -1,12 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC_databaskonstruktion.Models;
+using System.Data;
 
 namespace MVC_databaskonstruktion.Controllers
 {
     public class AgentsController : Controller
     {
+        private readonly IConfiguration _configuration;
+        private AgentsModel _agentsModel;
+
+        public AgentsController(IConfiguration configuration) 
+        {
+            this._configuration = configuration;
+        }
+
         public IActionResult Index()
         {
+            GetAgents();
             return View();
+        }
+
+        private void GetAgents()
+        {
+            _agentsModel = new AgentsModel(this._configuration);
+            DataTable agents = _agentsModel.GetAgents();
+            ViewBag.Agents = agents;
         }
     }
 }
---
+++ MVC-databaskonstruktion/Models/AgentsModel.cs
@@ -0,0 +1,31 @@
+﻿using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Security;
+using System.Data;
+
+namespace MVC_databaskonstruktion.Models
+{
+    public class AgentsModel
+    {
+        private IConfiguration _configuration;
+        private string connectionString;
+
+        public AgentsModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            this.connectionString = _configuration["ConnectionString"];
+        }
+
+        public DataTable GetAgents()
+        {
+            MySqlConnection dbcon = new MySqlConnection(connectionString);
+            dbcon.Open();
+            MySqlDataAdapter adapter = new MySqlDataAdapter("SELECT * FROM Agent;", dbcon);
+            DataSet ds = new DataSet();
+            adapter.Fill(ds, "result");
+            DataTable agentsTable = ds.Tables["result"];
+            dbcon.Close();
+
+            return agentsTable;
+        }
+    }
+}
