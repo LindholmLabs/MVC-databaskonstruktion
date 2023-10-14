a82767dceebe3bf2abb45677ac17948896f562d3
William Lindholm <william_lindholm@outlook.com>
2023-10-14 22:55:50 +0200

    Added Incidents page.

---
+++ MVC-databaskonstruktion/Controllers/IncidentsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_databaskonstruktion.Models;
+using MVC_databaskonstruktion.Utils;
+using MySql.Data.MySqlClient;
+using System.Diagnostics;
+
+namespace MVC_databaskonstruktion.Controllers
+{
+    public class IncidentsController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private IncidentsModel _incidentsModel { get; set; }
+
+        public IncidentsController(IConfiguration configuration)
+        {
+            this._configuration = configuration;
+            _incidentsModel = new IncidentsModel(_configuration);
+        }
+
+        public IActionResult Index(string IncidentName = "")
+        {
+            ViewBag.Table = _incidentsModel.GetIncidents(IncidentName);
+
+            return View();
+        }
+
+        public IActionResult Delete(string table, string incidentName, string incidentNumber)
+        {
+            try
+            {
+                _incidentsModel.DeleteIncident(table, incidentName, incidentNumber);
+            }
+            catch (MySqlException e)
+            {
+                switch (e.Number)
+                {
+                    case 1451:
+                        TempData["ErrorMessage"] = "Foreign Key Constraint Failed!";
+                        break;
+                    case 1452:
+                        TempData["ErrorMessage"] = "Foreign Key Constraint Failed!";
+                        break;
+                    default:
+                        TempData["ErrorMessage"] = $"Something went wrong: {e.Number}";
+                        break;
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVC-databaskonstruktion/Models/IncidentsModel.cs
@@ -0,0 +1,38 @@
+﻿using MVC_databaskonstruktion.Utils;
+
+namespace MVC_databaskonstruktion.Models
+{
+    public class IncidentsModel
+    {
+        private IConfiguration _configuration { get; set; }
+        private DatabaseRepository _databaseRepository { get; set; }
+        private TableObjectBuilder _tableBuilder { get; set; }
+
+        public IncidentsModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _databaseRepository = new DatabaseRepository(_configuration);
+            _tableBuilder = new TableObjectBuilder()
+                .SetPrimaryKeys(new List<string> { "IncidentName", "IncidentNumber" })
+                .SetDeleteTable("Incident");
+        }
+
+        public TableObject GetIncidents(string searchQuery)
+        {
+            return _tableBuilder
+                .SetDataTable(_databaseRepository.GetTable($"SELECT * FROM Incident WHERE IncidentName LIKE '%{searchQuery}%'"))
+                .Build();
+        }
+
+        public void DeleteIncident(string table, string incidentName, string incidentNumber)
+        {
+            List<KeyValuePair<string, string>> conditions = new List<KeyValuePair<string, string>>
+            {
+                new KeyValuePair<string, string>("IncidentName", incidentName),
+                new KeyValuePair<string, string>("IncidentNumber", incidentNumber)
+            };
+
+            _databaseRepository.DeleteRow(table, conditions);
+        }
+    }
+}
